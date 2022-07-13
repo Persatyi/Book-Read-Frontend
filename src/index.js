f8e806ff8a83d20a31d1b082ac449a14d0018207
Oleksii Persatyi <persatyi@ukr.net>
2022-07-13 14:15:38 +0300

    removed dot

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
+import { store, persistor } from "redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 import "modern-normalize";
