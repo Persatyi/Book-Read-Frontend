bfd9086e9c8954030a10a3b0952afef9c8bf7c87
Oleksii Persatyi <persatyi@ukr.net>
2022-07-14 14:42:33 +0300

    update

--- src/components/AddPages/AddPages.jsx
+++ src/components/AddPages/AddPages.jsx
@@ -1,21 +1,26 @@
 import s from "./AddPages.module.scss";
-import { Formik } from "formik";
+import { Formik, Form } from "formik";
 import DatePickerField from "components/DatePickerField";
 
 const AddPages = () => {
+  const onSubmit = () => {};
   return (
-    <Formik className={s.form}>
-      <h2>Results</h2>
-      <div className={s.wrapper}>
-        <div>
-          <p>Date</p>
-          <DatePickerField />
-        </div>
-        <div className={s.amountWrapper}>
-          <p>Amount of pages</p>
-          <input className={s.input} type="number" />
-        </div>
-      </div>
+    <Formik initialValues={{ date: "", pages: "" }} onSubmit={onSubmit}>
+      {({ values }) => {
+        <Form className={s.form}>
+          <h2>Results</h2>
+          <div className={s.wrapper}>
+            <div>
+              <p>Date</p>
+              <DatePickerField selected={new Date()} />
+            </div>
+            <div className={s.amountWrapper}>
+              <p>Amount of pages</p>
+              <input className={s.input} type="number" />
+            </div>
+          </div>
+        </Form>;
+      }}
     </Formik>
   );
 };
