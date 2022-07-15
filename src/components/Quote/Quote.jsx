8c7bc141c97b5859a631197d17f26f3d2c06579c
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2022-07-16 01:04:52 +0300

    add Quote component

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -1,6 +1,13 @@
-// import s from './Quote';
+import s from "./Quote";
 const Quote = () => {
-  return <div>Quote</div>;
+  return (
+    <div>
+      <blockquote className={s.quote}>
+        Books are the ships of thoughts, wandering through the waves of time.
+      </blockquote>
+      <p className={s.author}>Francis Bacon</p>
+    </div>
+  );
 };
 
 export default Quote;
