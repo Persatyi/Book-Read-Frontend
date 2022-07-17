348edb6b45b0aaa5946ecaa3a454461541b0f324
Oleksii Persatyi <persatyi@ukr.net>
2022-07-17 14:46:58 +0300

    update

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,40 +3,42 @@ import { createPortal } from "react-dom";
 import PropTypes from "prop-types";
 import s from "./Modal.module.scss";
 
-const modalRoot = document.querySelector('#modal-root');
-const Modal = ({ children, closeModal, overClass='', modalClass='' }) => {
-   const closeModalByEsc = useCallback(
-   (e) => {
+const modalRoot = document.querySelector("#modal-root");
+const Modal = ({ children, closeModal, overClass = "", modalClass = "" }) => {
+  const closeModalByEsc = useCallback(
+    (e) => {
       if (e.code === "Escape") {
-         closeModal();
+        closeModal();
       }
-   },
-   [closeModal]
-);
+    },
+    [closeModal]
+  );
 
-   const closeByBackdropClick = (e) => {
-      if (e.target === e.currentTarget) {
+  const closeByBackdropClick = (e) => {
+    if (e.target === e.currentTarget) {
       closeModal();
-   }
-   };
+    }
+  };
 
-   useEffect(() => {
-   window.addEventListener("keydown", closeModalByEsc);
-      return () => {
+  useEffect(() => {
+    window.addEventListener("keydown", closeModalByEsc);
+    return () => {
       window.removeEventListener("keydown", closeModalByEsc);
-      };
-   }, [closeModalByEsc]);
-return createPortal(
-   <div className={`${s.overlay} ${overClass}`} onClick={closeByBackdropClick}>
+    };
+  }, [closeModalByEsc]);
+  return createPortal(
+    <div className={`${s.overlay} ${overClass}`} onClick={closeByBackdropClick}>
       <div className={`${s.modal} ${modalClass}`}>{children}</div>
-   </div>,
-   modalRoot,
-);
+    </div>,
+    modalRoot
+  );
 };
 
 Modal.propTypes = {
-children: PropTypes.node.isRequired,
-closeModal: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+  closeModal: PropTypes.func.isRequired,
+  overClass: PropTypes.string,
+  modalClass: PropTypes.string,
 };
 
 export default Modal;
--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,76 +1,80 @@
-import React, {useState} from 'react'
-import { useSelector, useDispatch} from 'react-redux';
-import useWindowSize from 'hooks/use-winsow-size';
-import Library from "components/Library/Library"
-import LibraryModal from 'components/LibraryModal/LibraryModal';
-import globalSelectors from '../redux/global/global-selectors';
-import { toggleModal } from 'redux/global/global-slice';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useWindowSize } from "hooks/useWindowSize";
+import Library from "components/Library/Library";
+import LibraryModal from "components/LibraryModal/LibraryModal";
+import globalSelectors from "../redux/global/global-selectors";
+import { toggleModal } from "redux/global/global-slice";
 import spriteSvg from "assets/images/sprite.svg";
-import BookList from 'components/BookList/BookList';
-import Container from '../components/Container/Container';
+import BookList from "components/BookList/BookList";
+import Container from "../components/Container/Container";
 
 const moreIcon = {
   width: "52px",
   height: "52px",
-  position: 'fixed',
-  bottom: '12px',
-  cursor: 'pointer',
-  marginLeft: 'auto',
-  marginRight: 'auto',
+  position: "fixed",
+  bottom: "12px",
+  cursor: "pointer",
+  marginLeft: "auto",
+  marginRight: "auto",
   left: 0,
   right: 0,
-}
+};
 const backIcon = {
   marginTop: "24px",
   width: "24px",
-  height: "12px"
-}
+  height: "12px",
+};
 
 export default function LibraryPage() {
   const [openLibrary, setOpenLibrary] = useState(false);
   const [plus, setPlus] = useState(true);
   const dispatch = useDispatch();
   const size = useWindowSize();
-  const modalOpen = useSelector(globalSelectors.getIsModal)
+  const modalOpen = useSelector(globalSelectors.getIsModal);
   const isOpenModal = () => {
-    dispatch(toggleModal())
-  }
+    dispatch(toggleModal());
+  };
   const isLibraryToggle = () => {
-    setOpenLibrary(!openLibrary)
-    setPlus(!plus)
-  }
+    setOpenLibrary(!openLibrary);
+    setPlus(!plus);
+  };
   return (
-    <div style={{
-      backgroundColor: 'var(--secondary-bg-cl)',
-      minHeight: '92vh'
-    }}>
+    <div
+      style={{
+        backgroundColor: "var(--secondary-bg-cl)",
+        minHeight: "92vh",
+      }}
+    >
       <Container>
-      {size.width < 768 && (
-        <>
-          {plus &&
-          <><svg onClick={isLibraryToggle} style={moreIcon}>
-                <use href={`${spriteSvg}#icon-more`} />
-              </svg><BookList onClick={isOpenModal} /></>
-          }
-          {openLibrary && (
-            <>
-          <svg onClick={isLibraryToggle} style={backIcon}>
-            <use href={`${spriteSvg}#icon-back`}/>
-          </svg>
-              <Library />
-            </>
-          )}
-      </>
-      )}
-      {size.width > 768 &&  (
-        <>
-        <Library />
-          <BookList onClick={isOpenModal} />
-        </>
-      )}
-      {modalOpen && <LibraryModal />}
-    </Container>
-      </div>
-  )
-};
-
+        {size.width < 768 && (
+          <>
+            {plus && (
+              <>
+                <svg onClick={isLibraryToggle} style={moreIcon}>
+                  <use href={`${spriteSvg}#icon-more`} />
+                </svg>
+                <BookList onClick={isOpenModal} />
+              </>
+            )}
+            {openLibrary && (
+              <>
+                <svg onClick={isLibraryToggle} style={backIcon}>
+                  <use href={`${spriteSvg}#icon-back`} />
+                </svg>
+                <Library />
+              </>
+            )}
+          </>
+        )}
+        {size.width > 768 && (
+          <>
+            <Library />
+            <BookList onClick={isOpenModal} />
+          </>
+        )}
+        {modalOpen && <LibraryModal />}
+      </Container>
+    </div>
+  );
+}
