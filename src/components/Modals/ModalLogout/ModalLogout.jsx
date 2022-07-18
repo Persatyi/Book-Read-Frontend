f44a9896561be715f44162b77e397b770650a1c8
Selim Fazylov <selim.fazylov1@gmail.com>
2022-07-19 00:08:32 +0300

    modal text

--- src/components/Modals/ModalLogout/ModalLogout.jsx
+++ src/components/Modals/ModalLogout/ModalLogout.jsx
@@ -1,2 +1,17 @@
+import Button from "components/Button";
 import s from "./ModalLogout.module.scss";
 
+const ModalLogout = () => {
+    return (
+        <div className={s.modal}>
+            <p className={s.text}>Якщо Ви вийдете з програми незбережені дані будуть втрачені</p>
+            <div className={s.btnBox}>
+                <Button className={s} type={"button"} text={"Відміна"} onClick="" />
+                <Button className={s} type={"button"} text={"Вийти"} onClick="" />
+            </div>
+            
+        </div>
+    )
+};
+
+export default ModalLogout;
