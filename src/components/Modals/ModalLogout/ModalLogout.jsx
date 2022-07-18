8d84ef0f9e32a7d971e51fc999a80777b7b033b5
Selim Fazylov <selim.fazylov1@gmail.com>
2022-07-19 02:23:50 +0300

    add styles and finish

--- src/components/Modals/ModalLogout/ModalLogout.jsx
+++ src/components/Modals/ModalLogout/ModalLogout.jsx
@@ -1,17 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
 import Button from "components/Button";
+import ModalWrapper from "components/ModalWrapper";
 import s from "./ModalLogout.module.scss";
 
-const ModalLogout = () => {
+const ModalLogout = ({ open, onClose, logoutFunc }) => {
+
     return (
-        <div className={s.modal}>
-            <p className={s.text}>Якщо Ви вийдете з програми незбережені дані будуть втрачені</p>
-            <div className={s.btnBox}>
-                <Button className={s} type={"button"} text={"Відміна"} onClick="" />
-                <Button className={s} type={"button"} text={"Вийти"} onClick="" />
+        <ModalWrapper open={open} onClose={onClose}>
+            <div className={s.modal}>
+                <p className={s.text}>Якщо Ви вийдете з програми незбережені дані будуть втрачені</p>
+                <div className={s.btnBox}>
+                    <Button className={s.button} type={"button"} styleType="transparent" text={"Відміна"} onClick={onClose} />
+                    <Button className={s.button} type={"button"} text={"Вийти"} onClick={() => logoutFunc()} />
+                </div>
             </div>
-            
-        </div>
+        </ModalWrapper>
     )
+};
+
+ModalLogout.propTypes = {
+    open: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired,
+    logoutFunc: PropTypes.func.isRequired
 };
 
 export default ModalLogout;
--- src/components/Modals/index.js
+++ src/components/Modals/index.js
@@ -2,5 +2,6 @@ import ModalBookRead from "./ModalBookRead";
 import ModalChangesLost from "./ModalChangesLost";
 import ModalBookReview from "./ModalReview"
 import ModalTrainingDone from "./ModalTrainingDone";
+import ModalLogout from "./ModalLogout";
 
-export { ModalBookRead, ModalChangesLost, ModalBookReview, ModalTrainingDone };
+export { ModalBookRead, ModalChangesLost, ModalBookReview, ModalTrainingDone, ModalLogout };
