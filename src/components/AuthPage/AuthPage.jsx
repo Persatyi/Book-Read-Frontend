ab4e6cb786b57b64cdfef1454f8d9bd31b89e693
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2022-07-18 13:19:55 +0300

    second layout option

--- src/components/AuthPage/AuthPage.jsx
+++ src/components/AuthPage/AuthPage.jsx
@@ -10,7 +10,11 @@ const AuthPage = ({ type, children }) => {
 
   const adjustClassName = () => {
     const classNames = [s.formWrapper];
-    if (type === authType.registration && size.width < 768) {
+    if (
+      type === authType.registration &&
+      size.width < 768 &&
+      size.height > 592
+    ) {
       classNames.push(s.registrationMobile);
     }
     return classNames.join(" ");
@@ -29,6 +33,22 @@ const AuthPage = ({ type, children }) => {
         </div>
       )}
     </div>
+
+    // <div className={s.wrapperMain}>
+    //   <Container>
+    //     <div className={s.wrapperS}>
+    //       <div className={s.formWrapperS}>
+    //         <div className={s.bcg}>
+    //           <AuthForm type={type} />
+    //         </div>
+    //       </div>
+
+    //       {type === authType.registration && size.width < 768 ? null : (
+    //         <div className={s.textWrapperS}>{children}</div>
+    //       )}
+    //     </div>
+    //   </Container>
+    // </div>
   );
 };
 
