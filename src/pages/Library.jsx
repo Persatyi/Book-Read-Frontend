c135f500edb5684eb9994c72f15d687b93c23bc8
Yevhen Malyshko <memx86@gmail.com>
2022-07-19 11:20:05 +0300

    Library placholder fix

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,61 +1,70 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { isAuth } from "redux/auth";
-import {useBooksQuery} from 'redux/api/bookAPI'
+import { useBooksQuery } from "redux/api/bookAPI";
 import { useToggle, useWindowSize } from "hooks";
 import BookListLibrary from "components/BookListLibrary";
 import Container from "components/Container/Container";
 import MoreBtn from "components/MoreBtn/MoreBtn";
 import BackBtn from "components/BackBtn/BackBtn";
 import LibraryEmpty from "components/LibraryEmpty";
-import AddBook from 'components/AddBook';
+import AddBook from "components/AddBook";
 import { ModalBookReview } from "components/Modals";
 
 export default function LibraryPage() {
   const auth = useSelector(isAuth);
-  const { data = []} = useBooksQuery(null, { skip: !auth });
+  const {
+    data = [],
+    isSuccess,
+    isLoading,
+  } = useBooksQuery(null, { skip: !auth });
   const size = useWindowSize();
   const [plus, togglePlus] = useToggle();
   /* TODO: placeholder must be shown only when library is empty */
-  const [openPlaceholder, setOpenPlaceholder] = useState(true);
+  const [openPlaceholder, setOpenPlaceholder] = useState(false);
   const [openModal, toggleModal] = useToggle();
-  return (
-    <Container>
-      {size.width < 768 && (
-        <>
-              {plus && (
-                <>
+  useEffect(() => {
+    if (!data.length) {
+      setOpenPlaceholder(true);
+    } else setOpenPlaceholder(false);
+  }, [data.length]);
+  if (isLoading) return <div>Loading...</div>;
+  if (isSuccess)
+    return (
+      <Container>
+        {size.width < 768 && (
+          <>
+            {plus && (
+              <>
                 <MoreBtn onClick={togglePlus} />
-              {/* TODO: open Resume modal on click */}
-              <BookListLibrary onClick={() => { }} />
-            </>
-              )}
-          {!plus && (
-                <>
-              <BackBtn onClick={togglePlus} />
-              {!data.length &&
+                {/* TODO: open Resume modal on click */}
+                <BookListLibrary onClick={() => {}} />
+              </>
+            )}
+            {!plus && (
+              <>
+                <BackBtn onClick={togglePlus} />
                 <LibraryEmpty
                   open={openPlaceholder}
                   onClose={() => setOpenPlaceholder(false)}
-                />}
-              <AddBook />
-                </>
-          )}
-        </>
-      )}
-      {size.width >= 768 && (
-        <>
+                />
+                <AddBook />
+              </>
+            )}
+          </>
+        )}
+        {size.width >= 768 && (
+          <>
             <AddBook />
-          {/* TODO: open Resume modal on click */}
-          <BookListLibrary onClick={toggleModal} />
-            {!data.length &&
-                <LibraryEmpty
-                  open={openPlaceholder}
-                  onClose={() => setOpenPlaceholder(false)}
-                />}
-      <ModalBookReview open={openModal} onClose={toggleModal} />
-        </>
-      )}
-    </Container>
-  );
+            {/* TODO: open Resume modal on click */}
+            <BookListLibrary onClick={toggleModal} />
+            <LibraryEmpty
+              open={openPlaceholder}
+              onClose={() => setOpenPlaceholder(false)}
+            />
+            <ModalBookReview open={openModal} onClose={toggleModal} />
+          </>
+        )}
+      </Container>
+    );
 }
