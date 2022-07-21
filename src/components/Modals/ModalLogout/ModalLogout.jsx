79a6c22119fd9a27e5ef801d6a6eb59c8cc64b94
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2022-07-21 23:44:03 +0300

    remove extra digit

--- src/components/Modals/ModalLogout/ModalLogout.jsx
+++ src/components/Modals/ModalLogout/ModalLogout.jsx
@@ -1,4 +1,4 @@
-import React, from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Button from "components/Button";
 import ModalWrapper from "components/ModalWrapper";
