aeb3d2ccae079c435a2e2e28e71df5ca22210946
Yevhen Malyshko <memx86@gmail.com>
2022-07-22 19:02:52 +0300

    LineChart localization fix, Loader localization

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,14 @@
-import s from "./Loader.module.scss";
 import { Rings } from "react-loader-spinner";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
+import useTranslation from "hooks/useTranslation";
+import s from "./Loader.module.scss";
+
 const Loader = () => {
+  const { t } = useTranslation("Loader");
   return (
     <div className={s.backdrop}>
-      <Rings height="200" width="200" color="#ff6b08" ariaLabel="loading" />
+      <Rings height="200" width="200" color="#ff6b08" ariaLabel={t.label} />
     </div>
   );
 };
