aff3b1405261e6db20c5cf10783420eb9bc1af70
Selim Fazylov <selim.fazylov1@gmail.com>
2022-07-22 12:24:06 +0300

    worked code

--- src/components/GoogleBtn/GoogleBtn.jsx
+++ src/components/GoogleBtn/GoogleBtn.jsx
@@ -1,5 +1,5 @@
 import s from "./GoogleBtn.module.scss";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import jwt_decode from "jwt-decode";
 // import {GoogleLogin} from "react-google-login";
 import { toast } from "react-toastify";
@@ -9,45 +9,25 @@ import { loggedIn } from "redux/auth";
 
 
 const GoogleBtn = () => {
-
-    // const handleFailure = (result) => {
-    //     console.log(result)
-    //     toast.error("Something went wrong! Try again!");
-    // }
-
-    // const handleLogin = (data) => {
-    //     console.log(data)
-    // }
-
     const dispatch = useDispatch();
     const [googleLoginUser] = useGoogleLoginMutation();
 
-    const toSendToken = async (token) => {
-        try {
-            const response = await googleLoginUser(token).unwrap();
-
-            dispatch(loggedIn(response.token));
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
     function handleCallback(resp) {
-        console.log("JWT: ", typeof resp.credential);
-        const userObject = jwt_decode(resp.credential);
-        console.log(userObject);
         
         const googleToken = resp.credential;
+        console.log("google Token:", googleToken)
+        
         const googleObj = {
             googleToken
         }
-        toSendToken(googleObj);
+        googleLoginUser(googleObj).unwrap().then((response) => dispatch(loggedIn(response.token))).catch(error => console.log(error))
     }
+    
 
     useEffect(() => {
         window.google.accounts.id.initialize({
             client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-            callback: handleCallback
+            callback: handleCallback 
         });
 
         window.google.accounts.id.renderButton(
