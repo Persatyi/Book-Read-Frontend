24b15c8930f5dfd688c25adfe302dca9f71a5301
Yevhen Malyshko <memx86@gmail.com>
2022-07-24 22:07:04 +0300

    types for Loader component, loader for training and library page

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,10 +4,15 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import useTranslation from "hooks/useTranslation";
 import s from "./Loader.module.scss";
 
-const Loader = () => {
+export const TYPES = {
+  LOCAL: "local",
+  FULLSCREEN: "fullscreen",
+};
+
+const Loader = ({ type = TYPES.LOCAL }) => {
   const { t } = useTranslation("Loader");
   return (
-    <div className={s.backdrop}>
+    <div className={s[type]}>
       <Rings height="200" width="200" color="#ff6b08" ariaLabel={t.label} />
     </div>
   );
--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,1 +1,1 @@
-export { default } from "./Loader";
+export { default, TYPES } from "./Loader";
