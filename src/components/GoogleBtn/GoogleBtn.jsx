545ca2d212c7c92bca4e57b7a41c9badf1d29473
Oleksii Persatyi <persatyi@ukr.net>
2022-07-24 12:31:39 +0300

    fixed google button display

--- src/components/GoogleBtn/GoogleBtn.jsx
+++ src/components/GoogleBtn/GoogleBtn.jsx
@@ -5,47 +5,60 @@ import { useGoogleLoginMutation } from "redux/api/bookAPI";
 import { useDispatch } from "react-redux";
 import { loggedIn } from "redux/auth";
 
-
 const GoogleBtn = () => {
-    const dispatch = useDispatch();
-    const [googleLoginUser] = useGoogleLoginMutation();
+  const dispatch = useDispatch();
+  const [googleLoginUser] = useGoogleLoginMutation();
 
-    function handleCallback(resp) {
-        const googleToken = resp.credential;
-        console.log("google Token:", googleToken)
-        
-        const googleObj = {
-            googleToken
+  function handleCallback(resp) {
+    const googleToken = resp.credential;
+
+    const googleObj = {
+      googleToken,
+    };
+    googleLoginUser(googleObj)
+      .unwrap()
+      .then((response) =>
+        dispatch(
+          loggedIn({
+            token: response.token,
+            refreshToken: response.refreshToken,
+          })
+        )
+      )
+      .catch((error) => {
+        if (error.status === 409) {
+          toast.error(
+            "Account with this email was ceated with a different signup method!"
+          );
         }
-        googleLoginUser(googleObj).unwrap()
-            .then((response) => dispatch(loggedIn({
-                token: response.token,
-                refreshToken: response.refreshToken
-            })))
-            .catch(error => {
-                if (error.status === 409) {
-                    toast.error("Account with this email was ceated with a different signup method!");
-                }
-            });
-    }
-    
+      });
+  }
 
-    useEffect(() => {
-        window.google.accounts.id.initialize({
-            client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-            callback: handleCallback
-        });
+  useEffect(() => {
+    window.google.accounts.id.initialize({
+      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      callback: handleCallback,
+    });
 
-        window.google.accounts.id.renderButton(
-            document.getElementById("signInBtn"),
-            { type: "standard", theme: "outline", size: "medium", width: "40", height: "50", locale: "en",  }
-        );
+    window.google.accounts.id.renderButton(
+      document.getElementById("signInBtn"),
+      {
+        type: "standard",
+        theme: "outline",
+        size: "medium",
+        width: "40",
+        height: "50",
+        locale: "en",
+      }
+    );
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+  }, []);
 
-    return (
-        <div id="signInBtn" className={s.signInBtn}></div>
-    )
-}
+  return (
+    <span className={s.wrapper}>
+      <div id="signInBtn"></div>
+    </span>
+  );
+};
 
 export default GoogleBtn;
