ded556a7beca2a9fd5ad8f8f886400761ebdcf26
Yevhen Malyshko <memx86@gmail.com>
2022-07-26 14:56:16 +0300

    QueryErrorHandler dispatches logout on error count > retries

--- src/components/QueryErrorHandler.jsx
+++ src/components/QueryErrorHandler.jsx
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
+import PropTypes from "prop-types";
 
 import { useDispatch, useSelector } from "react-redux";
 import { useBooksQuery, useCurrentQuery } from "redux/api/bookAPI";
@@ -9,7 +10,7 @@ import { isAuth, loggedOff } from "redux/auth";
 import { getTraining, getResults } from "services";
 import useRefreshToken from "hooks/useRefreshToken";
 
-const QueryErrorHandler = () => {
+const QueryErrorHandler = ({ retries = 1 }) => {
   const auth = useSelector(isAuth);
   const dispatch = useDispatch();
   const checkRefreshToken = useRefreshToken();
@@ -24,12 +25,17 @@ const QueryErrorHandler = () => {
     enabled: isToken && auth,
     retry: false,
   });
+  const errorCounter = useRef(0);
 
   const queryError =
     currentError || booksError || trainingError || resultsError;
 
   useEffect(() => {
     if (!queryError) return;
+
+    errorCounter.current += 1;
+    if (errorCounter.current > retries) dispatch(loggedOff());
+
     (async () => {
       try {
         await checkRefreshToken();
@@ -41,4 +47,8 @@ const QueryErrorHandler = () => {
   }, [queryError]);
   return null;
 };
+QueryErrorHandler.propTypes = {
+  retries: PropTypes.number,
+};
+
 export default QueryErrorHandler;
